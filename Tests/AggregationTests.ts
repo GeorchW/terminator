29205aaff11797a96e2b1e5f0315a11cec744bba
Georg Wicke <georg.wicke@rwth-aachen.de>
2018-12-03 18:21:13 +0100

    Added proper test setup

---
+++ Tests/AggregationTests.ts
@@ -0,0 +1,51 @@
+describe('Sum', () => {
+    it('should implement symbolic equality properly', () => {
+        const sum1 = new Sum([new MathSymbol("a"), new MathSymbol("b")])
+        const sum2 = new Sum([new MathSymbol("a"), new MathSymbol("b")])
+        expect(sum1.equals(sum2)).toBeTruthy()
+    });
+    it('should merge (simple symbol) summands', () => {
+        const sum = new Sum([new MathSymbol("a"), new MathSymbol("a")])
+        expect(sum).toEqual(new Sum([new AbelianTermItem(2, new MathSymbol("a"))]));
+    });
+    it('should merge (simple symbol) summands with different factors', () => {
+        const sum = new Sum([new MathSymbol("a"), new AbelianTermItem(3, new MathSymbol("a"))])
+        expect(sum).toEqual(new Sum([new AbelianTermItem(4, new MathSymbol("a"))]));
+    });
+    it('should merge (factor) summands', () => {
+        const sum = new Sum([new Product([new MathSymbol("a"), new MathSymbol("b")]), new Product([new MathSymbol("a"), new MathSymbol("b")])])
+        const expected = new Sum([new AbelianTermItem(2, new Product([new MathSymbol("a"), new MathSymbol("b")]))]);
+        expect(sum.toString()).toEqual(expected.toString())
+        expect(sum).toEqual(expected);
+    });
+});
+
+describe("OrderedFrozenSet", ()=>{
+    describe("equals", () => {
+        it("should recognize that two instances of an empty set are equal", () => {
+            const a = new OrderedFrozenSet([])
+            const b = new OrderedFrozenSet([])
+            expect(a.equals(b)).toBeTruthy()
+        })
+        it("should recognize equality of one-item sets", () => {
+            const a = new OrderedFrozenSet([new Constant(1)])
+            const b = new OrderedFrozenSet([new Constant(1)])
+            expect(a.equals(b)).toBeTruthy()
+        })
+        it("should recognize basic equality", () => {
+            const a = new OrderedFrozenSet([new Constant(1), new Constant(2), new Constant(3)])
+            const b = new OrderedFrozenSet([new Constant(1), new Constant(2), new Constant(3)])
+            expect(a.equals(b)).toBeTruthy()
+        })
+        it("should recognize equality of ordered inputs", () => {
+            const a = new OrderedFrozenSet([new Constant(1), new Constant(2), new Constant(3)])
+            const b = new OrderedFrozenSet([new Constant(1), new Constant(2), new Constant(3)])
+            expect(a.equals(b)).toBeTruthy()
+        })
+        it("should recognize equality independent of order", () => {
+            const a = new OrderedFrozenSet([new Constant(1), new Constant(2), new Constant(3)])
+            const b = new OrderedFrozenSet([new Constant(2), new Constant(3), new Constant(1)])
+            expect(a.equals(b)).toBeTruthy()
+        })
+    })
+})
