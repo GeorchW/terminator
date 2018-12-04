aa83209550b0eb05e1a29b4af1ca73e655c96daf
Georg Wicke <georg.wicke@rwth-aachen.de>
2018-12-04 01:05:25 +0100

    Fixed neutral elements not being removed

--- Reductions/removeNeutralElements.ts
+++ Reductions/removeNeutralElements.ts
@@ -3,8 +3,8 @@ function removeNeutralElements(term: Term): Term {
         let newTerms = [];
         let hasChange = false;
         for (const subterm of term.terms.array)
-            if (subterm.actualTerm instanceof Constant)
-                newTerms.push(new AbelianTermItem(subterm.actualTerm.value * subterm.constantModifier, new Constant(term.neutralElement)));
+            if (subterm.actualTerm instanceof Constant && subterm.actualTerm.value == term.neutralElement)
+                hasChange = true
             else
                 newTerms.push(subterm);
         if (hasChange)
