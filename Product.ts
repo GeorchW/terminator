3999b2fcc45601ec167a7748c816fd709504faf3
Georg Wicke <georg.wicke@rwth-aachen.de>
2018-12-04 02:12:44 +0100

    Lots of beautifications, including #18 (negative potences as fractions)

--- Product.ts
+++ Product.ts
@@ -16,6 +16,42 @@ class Product extends AbelianTerm {
         else
             return $("<span/>").append(baseTerm, $("<sup/>").append(item.constantModifier.toString()))
     }
+    public toDisplayable(params:DisplayParams): JQuery<HTMLElement> {
+        let result = $("<span/>").attr("class", "product")
+        if (this.terms.array.length == 0)
+            result.append(this.neutralElement.toString())
+        else
+        {
+            const targetDividend = {target:$("<span/>"), empty:true}
+            const targetDivisor = {target:$("<span/>"), empty:true}
+            for (const term of this.terms.array) {
+                const target = term.constantModifier > 0 ? targetDividend : targetDivisor
+                if(target.empty) target.empty = false
+                else if (this.requiresOperationSymbol(term))
+                    target.target.append(params.preferString ? this.operationSymbol : this.operationSymbolHtml)
+                if (params.clickable)
+                    target.target.append(
+                        clickable(
+                            this.itemToDisplayable(
+                                new AbelianTermItem(
+                                    Math.abs(term.constantModifier),
+                                    term.actualTerm), 
+                                params.unclickable()), () => {
+                        if (context.currentEquation == undefined) return
+                        const newEquation = context.currentEquation.apply(this.getInverter(term))
+                        context.addNewEquation(newEquation)
+                    }))
+                else target.target.append(this.itemToDisplayable(term, params))
+            }
+            if(targetDivisor.empty) result.append(targetDividend.target)
+            else {
+                if(targetDividend.empty) targetDividend.target.append("1")
+                result.append(targetDividend.target, $("<hr/>"), targetDivisor.target)
+            }
+            
+        }
+        return result
+    }
     public createNew(terms: (AbelianTermItem | Term)[]): AbelianTerm {
         return new Product(terms)
     }
