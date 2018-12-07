ef56b0b0269a480622e30ac2228c8b50dc8e8400
Georg Wicke <georg.wicke@rwth-aachen.de>
2018-12-07 09:36:51 +0100

    Added package.json

--- clickable.ts
+++ clickable.ts
@@ -1,4 +1,4 @@
-function clickable(content: string | JQuery<HTMLElement>, handler: JQuery.EventHandler<HTMLElement> | JQuery.EventHandlerBase<any, JQuery.Event<HTMLElement>> | false = false): JQuery<HTMLElement> {
+function clickable(content: string | JQuery<HTMLElement>, handler: JQuery.EventHandler<HTMLElement> | JQuery.EventHandlerBase<any, JQuery.Event> | false = false): JQuery<HTMLElement> {
     let base = typeof content == "string"?
         $("<span/>").append(content):content
     return base
