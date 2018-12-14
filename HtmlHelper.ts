c4d9315837e1d206273aba87b4b56a92593e8dc8
GeorchW <georg.wicke@rwth-aachen.de>
2018-12-14 19:38:01 +0100

    Fixed click and mouseover propagation

--- HtmlHelper.ts
+++ HtmlHelper.ts
@@ -1,10 +1,18 @@
+var hoveredElement: JQuery<HTMLElement> | undefined = undefined
+
 function clickable(content: string | JQuery<HTMLElement>, handler: JQuery.EventHandler<HTMLElement> | JQuery.EventHandlerBase<any, JQuery.Event> | false = false): JQuery<HTMLElement> {
     let base = typeof content == "string" ?
         $("<span/>").append(content) : content
-    if(handler != false){
+    if (handler != false) {
         base
-            .addClass("clickable")
-            .on("click", handler)
+            .on("mouseover mouseout", e => {
+                base.toggleClass("clickableMouseOver", e.type === "mouseover")
+                e.stopPropagation()
+            })
+            .on("click", e => {
+                handler(e)
+                e.stopPropagation()
+            })
     }
     return base
 }
