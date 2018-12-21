a69f0fe1f0b959ae929a11aee111649355b1af8f
hannah arndt <hannah.arndt@rwth-aachen.de>
2018-12-21 21:45:20 +0100

    Added underscores rendering the rest as sub and alpha rendering as α

--- MathSymbol.ts
+++ MathSymbol.ts
@@ -10,7 +10,26 @@ class MathSymbol extends Term {
         super()
         this.hash = stringHash(name);
     }
-    public toDisplayable(): JQuery<HTMLElement> {
+    private convertToHtmlCharacter(x:string):string {
+        const entity = "&" + x + ";"
+        if($("<div/>").append(entity).text().length == 1)
+            return entity
+        else return x
+    }
+    public toDisplayable(params: DisplayParams): JQuery<HTMLElement> {
+        if (!params.preferString) {
+            const parts = this.name.split("_")
+            const mainText = parts[0]
+            const subTexts = parts.slice(1)
+            const result = $("<span/>").append(this.convertToHtmlCharacter(mainText));
+            var innermostElement = result
+            for (const subText of subTexts) {
+                const subElement = $("<sub/>").append(this.convertToHtmlCharacter(subText))
+                innermostElement.append(subElement)
+                innermostElement = subElement
+            }
+            return result
+        }
         return $("<span/>").append(this.name);
     }
     public checkReducable(): boolean {
